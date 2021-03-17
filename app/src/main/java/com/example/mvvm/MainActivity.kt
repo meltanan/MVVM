b742cb6eab42e6a0ae9a3edb036dcd9704a38637
Mohamed Eltanani <meltanani@Administrators-MacBook-Pro-3.local>
2021-03-17 13:54:18 -0400

    added internet connection successfully

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -1,32 +1,67 @@
 package com.example.mvvm
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
 
 
     private var idList = mutableListOf<String>();
+    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        run();
         postToList();
 
         recyclerViewId.layoutManager = LinearLayoutManager(this);
         recyclerViewId.adapter = RecyclerAdapter(idList);
 
+
+
+
+    }
+
+    fun run() {
+        val request = Request.Builder()
+            .url("https://publicobject.com/helloworld.txt")
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("demo", "${e.printStackTrace()}")
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    if (response.isSuccessful){
+                        Log.d("demo", "response is successful")
+                    }
+                    else {
+                        Log.d("demo", "response is NOT successful")
+                    }
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+
+                    println(response.body!!.string())
+                }
+            }
+        })
     }
 
     private fun addToList(id: String){
         idList.add(id);
-        Log.d("demo ", "here is it $idList")
     }
 
     private fun postToList(){

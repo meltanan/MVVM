5943db34c048f5f9681cbe25f0407b4bc761d680
Mohamed Eltanani <meltanani@AdministorsMBP3.carolina.rr.com>
2021-03-18 15:50:55 -0400

    added activity for retrofit

---
+++ app/src/main/java/com/example/mvvm/RetroFitActivityPra.kt
@@ -0,0 +1,11 @@
+package com.example.mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RetroFitActivityPra : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retro_fit_pra)
+    }
+}
